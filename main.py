6f637bbe6c9795e96f5e576ea70729d52c42da47
Diana Cardona <dscardona@gmail.com>
2023-05-31 19:55:21 -0400

    Add 4 turtles to race start, different colors

--- main.py
+++ main.py
@@ -4,9 +4,31 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
-cursor = Turtle(shape="turtle")
-cursor.penup()
-cursor.goto(x=-230, y=-100)
+blue = Turtle(shape="turtle")
+blue.penup()
+blue.goto(x=-230, y=70)
+blue.color("blue")
+
+green = Turtle(shape="turtle")
+green.penup()
+green.goto(x=-230, y=30)
+green.color("green")
+
+yellow = Turtle(shape="turtle")
+yellow.penup()
+yellow.goto(x=-230, y=-10)
+yellow.color("yellow")
+
+orange = Turtle(shape="turtle")
+orange.penup()
+orange.goto(x=-230, y=-50)
+orange.color("orange")
+
+red = Turtle(shape="turtle")
+red.penup()
+red.goto(x=-230, y=-90)
+red.color("red")
  
 screen.exitonclick()
