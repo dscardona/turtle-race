e194a0ff2371eef4d17a1a46f75f4622196bc854
Diana Cardona <dscardona@gmail.com>
2023-05-31 19:46:17 -0400

    Move one  cursor/turtle to beginning of race

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from turtle import Turtle, Screen
 
-cursor = Turtle()
+
 screen = Screen()
 screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 
-
+cursor = Turtle()
+cursor.penup()
+cursor.shape("turtle")
+cursor.goto(x=-230, y=-100)
+ 
 screen.exitonclick()
